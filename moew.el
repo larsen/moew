e02fdb1be58568570bb8c65f027daf214d4cf424
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2014-09-07 09:12:07 -0400

    Initial import

---
+++ moew.el
@@ -0,0 +1,37 @@
+;;; My Org Elnode webservice (moew)
+
+(defvar moew:org-dir "/home/larsen/Dropbox/org")
+(defvar moew:org-filename "mobile-notes.org")
+(defvar moew:port 8002)
+
+(defun moew:org-complete-filename ()
+  (format "%s/%s" moew:org-dir moew:org-filename))
+
+(defun moew:main-handler (httpcon)
+  (let ((form-page "form.html"))
+    (elnode-hostpath-dispatcher
+     httpcon
+     `(("^.*//form"     . moew:form)
+       ("^.*//keywords" . moew:keywords)
+       ("^.*//save"     . moew:save)))))
+
+(defun moew:form (httpcon)
+  (elnode-http-start httpcon 200 '("Content-type" . "text/html"))
+  (elnode-send-file httpcon "form.html"))
+
+(defun moew:keywords (httpcon)
+  (elnode-send-json httpcon
+                    (json-encode (cdr (car org-todo-keywords)))))
+
+(defun moew:todo-item-complete-text (keyword body)
+  (format "* %s %s\n" keyword body))
+
+(defun moew:save (httpcon)
+  (let ((todo-item-body (elnode-http-param httpcon "text"))
+        (todo-item-keyword (elnode-http-param httpcon "keyword")))
+    (append-to-file 
+     (moew:todo-item-complete-text todo-item-keyword todo-item-body)
+     nil (moew:org-complete-filename))
+    (elnode-send-json httpcon (json-encode "ok"))))
+
+(elnode-start 'moew:main-handler :port moew:port :host "*")
